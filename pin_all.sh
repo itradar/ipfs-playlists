07fac4ad0a39691483ec17d51a9e66c37f0c1916
itradar <raderic@outlook.com>
2016-04-10 12:27:13 +0200

    modified:   albums/Alumo - Electric (2015).xspf new file:   albums/Chronox - Voyager (2015).xspf new file:   albums/CloZee - Dubious EP (2012).xspf new file:   albums/CloZee - Jafump T, Eyes In Eyes (2013).xspf modified:   albums/CloZee - OckeFilms Soundtrack 2012 (2013).xspf modified:   albums/Diablo Swing Orchestra - The Butchers Ballroom (2009).xspf modified:   albums/LukHash - Falling Apart (2012).xspf modified:   albums/LukHash - The Other Side (2015).xspf modified:   albums/Lyvo - Chapter 1 (2014).xspf modified:   albums/Lyvo - Chapter 2 (2015).xspf new file:   albums/OMFG - OMFG (2015).xspf modified:   albums/Sweet Play - Colours Of The Day (2015).xspf new file:   albums/The Kyoto Connection - FATHER _ SON (2011).xspf new file:   albums/The Kyoto Connection - TEN (The Best Of) (2015).xspf new file:   albums/The Kyoto Connection - Wake Up (2013).xspf new file:   albums/tryad - listen (2006).xspf new file:   albums/tryad - public domain (2006).xspf new file:   albums/zero-project - Ambient symphony (2009).xspf new file:   albums/zero-project - Autumn prelude (2009).xspf new file:   albums/zero-project - Disabled emotions (2008).xspf new file:   albums/zero-project - Earth (2009).xspf new file:   albums/zero-project - Fairytale (2010).xspf new file:   albums/zero-project - Fairytale 2 (2010).xspf new file:   albums/zero-project - Fairytale 2_ The orchestral expansion (2010).xspf new file:   albums/zero-project - Metamorphosis (2015).xspf new file:   albums/zero-project - Untold stories of a dying moon (2012).xspf new file:   albums/zero-project - e-world_ The ultimate edition (2015).xspf modified:   pin_all.sh

--- pin_all.sh
+++ pin_all.sh
@@ -24,3 +24,60 @@ $IBIN pin add -r /ipfs/QmdWmWF1vN5fykDZt3MexyoPEPYVBFd1obYcdgMUfgBfBf/
 
 echo "PINNING: CloZee - OckeFilms Soundtrack 2012 (2013)"
 $IBIN pin add -r /ipfs/QmZqNn8ifNX5u6nqVrqP3hc4WYSqexTW61HKzT14ZGAqcJ/
+
+echo "PINNING: Chronox - Voyager (2015)"
+$IBIN pin add -r /ipfs/QmNsSeGtwNrFCk7LLufwhKsTWURHLbp1xSZP2ZiKUzAXzF/
+
+echo "PINNING: CloZee - Dubious EP (2012)"
+$IBIN pin add -r /ipfs/QmRgUPtcMzDZRmTSP71wDpVJUbDCEiLTdAB7H9ap5XYZRw/
+
+echo "PINNING: CloZee - Jafump T, Eyes In Eyes (2013)"
+$IBIN pin add -r /ipfs/QmPPzAeZq11GLqjjxAsXhswXftvZsKbQfbhA3pW5RLSjQX/
+
+echo "PINNING: OMFG - OMFG (2015)"
+$IBIN pin add -r /ipfs/QmUVQGZsoiEW3vquSQfUtdqqrWoLwNQhKKabMEA13r6kPy/
+
+echo "PINNING: tryad - listen (2006)"
+$IBIN pin add -r /ipfs/QmNk96TRKk72B3iimy7dh4RpD5TqKqAoSaVDUQtkVV6P47/
+
+echo "PINNING: tryad - public domain (2006)"
+$IBIN pin add -r /ipfs/QmYRDXgZER16MGUnK9cr7mb44LtCdUJB2WDikAKp8LPvjN/
+
+echo "PINNING: The Kyoto Connection - TEN (The Best Of) (2015)"
+$IBIN pin add -r /ipfs/QmcB8DKXf1aNzPhNHrhEoU7XWiFoC21LPLXH5yEN4XXdNd/
+
+echo "PINNING: The Kyoto Connection - Wake Up (2013)"
+$IBIN pin add -r /ipfs/QmUTJHwXrZfnq3uu1ezYUAjN5SUEmGuS7Rvc2DmCZfM7uv/
+
+echo "PINNING: The Kyoto Connection - FATHER _ SON (2011)"
+$IBIN pin add -r /ipfs/QmY4UpKhN1ExemGiL8CDkfZ34rNRbzToA21P5yDpZxnu4w/
+
+echo "PINNING: zero-project - Metamorphosis (2015)"
+$IBIN pin add -r /ipfs/QmQ1YmsgqcNEetNzNwVkyS4jWeHV9aoApDBKbszcuXEiec/
+
+echo "PINNING: zero-project - e-world: The ultimate edition (2015)"
+$IBIN pin add -r /ipfs/QmYtbjXaXejf89HUpj1rXgaGuYhyxC8ctR1gK6ZY9yWp8k/
+
+echo "PINNING: zero-project - Untold stories of a dying moon (2012)"
+$IBIN pin add -r /ipfs/QmZHTg1yBjjXKovxBwMoKWXt4Vz6mUDLq7sVjsX8AP4zjH/
+
+echo "PINNING: zero-project - Fairytale 2: The orchestral expansion (2010)"
+$IBIN pin add -r /ipfs/QmWPBJ5wUhfx1MEYjM1xn2Hoydo2GycqVx5QPC6ZGcTGHb/
+
+echo "PINNING: zero-project - Fairytale 2 (2010)"
+$IBIN pin add -r /ipfs/QmWxBb8Hyv6gTKR4Vuf41FEgy7oEMA9mYnbY8y2N6BfFpq/
+
+echo "PINNING: zero-project - Fairytale (2010)"
+$IBIN pin add -r /ipfs/QmfUFQurnDX7pCxK3VGYMQ8cQtpqvzTqExctwVfYP6Y3d3/
+
+echo "PINNING: zero-project - Ambient symphony (2009)"
+$IBIN pin add -r /ipfs/QmNbDMmkdYv5aps72nwFDBEb7t9eYqVNXjAXarczfboMi8/
+
+echo "PINNING: zero-project - Earth (2009)"
+$IBIN pin add -r /ipfs/Qmcbxuz54QoWcJiHHeNg2ubf6TYzBvALjk3x5gsPpYQyiL/
+
+echo "PINNING: zero-project - Autumn prelude (2009)"
+$IBIN pin add -r /ipfs/QmbZJ9nHBXugB4YweHNPHxyH7Agh5npG4PgYkp7cysGULt/
+
+echo "PINNING: zero-project - Disabled emotions (2008)"
+$IBIN pin add -r /ipfs/QmUkk3Hdkjhx2gaBPEZzbJK8wAnK2TGekwXdTqWbEwqJsR/
